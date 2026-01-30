bf4cf4df3afca5b3397525989fb3d3340bf3d67c
Felipe <felipedamatarodrigues@proton.me>
2026-01-30 12:18:19 -0300

    Implement basic TCP client in client.c

--- client/client.c
+++ client/client.c
@@ -1,1 +1,48 @@
-// Coming soon //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE];
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        return -1;
+    }
+
+    printf("Connected to the server\n\n");
+
+    while (1) {
+        
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+            printf("Error reading input.\n");
+            break;
+        }
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        fflush(stdin);
+        send(sock, buffer, strlen(buffer), 0);
+        printf("%s\n", buffer);
+    }
+    return 0;
+}
